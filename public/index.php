0a9247d3d5990a405be631847dac2348ca9884f9
Evgeny Kuzmin <skchel174@gmail.com>
2023-08-11 22:28:38 +0500

    Added sumple routing to index file

--- public/index.php
+++ public/index.php
@@ -1,3 +1,37 @@
 <?php
 
-echo 'Hello, World!';
+declare(strict_types=1);
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+
+define('PROJECT_DIR', dirname(__DIR__));
+
+require_once PROJECT_DIR . '/vendor/autoload.php';
+
+$routes = new RouteCollection();
+
+$routes->add('dummy', new Route('/', ['_controller' => function (): Response {
+    return new Response('Hello, World!');
+}]));
+
+$request = Request::createFromGlobals();
+$context = new RequestContext();
+$context->fromRequest($request);
+
+$urlMatcher = new UrlMatcher($routes, $context);
+
+try {
+    $parameters = $urlMatcher->match($context->getPathInfo());
+    $handler = $parameters['_controller'];
+    $response = $handler($request);
+} catch (ResourceNotFoundException $e) {
+    $response = new Response('404 error', 404);
+}
+
+$response->send();
