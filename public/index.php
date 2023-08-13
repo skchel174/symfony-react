2931774180bd404928813c99909dceef4ce5ce7c
Evgeny Kuzmin <skchel174@gmail.com>
2023-08-13 14:14:45 +0500

    Added container cache

--- public/index.php
+++ public/index.php
@@ -9,6 +9,7 @@ use App\Event\ResponseEvent;
 use Psr\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
 use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,34 +22,42 @@ require_once PROJECT_DIR . '/vendor/autoload.php';
 
 // Initializing container
 
-$parameters = new ParameterBag([
-    'env' => ENV,
-    'debug' => DEBUG,
-    'project_dir' => PROJECT_DIR,
-    'config_dir' => PROJECT_DIR . '/config',
-    'cache_dir' => PROJECT_DIR . '/var/cache/' . ENV
-]);
+$containerCache = PROJECT_DIR . '/var/cache/' . ENV . '/container.php';
 
-$container = new ContainerBuilder($parameters);
+if (file_exists($containerCache)) {
+    require_once $containerCache;
+    $container = new ProjectServiceContainer();
+} else {
+    $container = new ContainerBuilder(new ParameterBag([
+        'env' => ENV,
+        'debug' => DEBUG,
+        'project_dir' => PROJECT_DIR,
+        'config_dir' => PROJECT_DIR . '/config',
+        'cache_dir' => PROJECT_DIR . '/var/cache/' . ENV
+    ]));
 
-$extensions = require_once PROJECT_DIR . '/config/extensions.php';
+    $extensions = require_once PROJECT_DIR . '/config/extensions.php';
 
-foreach ($extensions as $extension) {
-    $container->registerExtension(new $extension());
-}
-
-$fileLocator = new FileLocator(PROJECT_DIR . '/config');
-$loader = new PhpFileLoader($container, $fileLocator);
-$loader->load('services.php');
-
-foreach ($container->getExtensions() as $extension) {
-    if (empty($container->getExtensionConfig($extension->getAlias()))) {
-        // Add empty extension configuration array if configuration is missing
-        $container->loadFromExtension($extension->getAlias(), []);
+    foreach ($extensions as $extension) {
+        $container->registerExtension(new $extension());
     }
-}
 
-$container->compile();
+    $fileLocator = new FileLocator(PROJECT_DIR . '/config');
+    $loader = new PhpFileLoader($container, $fileLocator);
+    $loader->load('services.php');
+
+    foreach ($container->getExtensions() as $extension) {
+        if (empty($container->getExtensionConfig($extension->getAlias()))) {
+            // Add empty extension configuration array if configuration is missing
+            $container->loadFromExtension($extension->getAlias(), []);
+        }
+    }
+
+    $container->compile();
+
+    $dumper = new PhpDumper($container);
+    file_put_contents($containerCache, $dumper->dump());
+}
 
 // Handle request
 
