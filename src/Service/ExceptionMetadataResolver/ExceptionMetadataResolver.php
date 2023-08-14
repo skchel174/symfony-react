1825ba57bdb906977095d4fdffbc2e490e1ebd6d
Evgeny Kuzmin <skchel174@gmail.com>
2023-08-14 18:14:54 +0500

    Created ExceptionMetadataResolver

---
+++ src/Service/ExceptionMetadataResolver/ExceptionMetadataResolver.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\ExceptionMetadataResolver;
+
+use Throwable;
+
+readonly class ExceptionMetadataResolver
+{
+    public function __construct(private array $exceptionsMapping = [])
+    {
+    }
+
+    public function resolve(Throwable $e): ExceptionMetadata
+    {
+        $options = $this->findOptionsByType($e);
+
+        $metadata = new ExceptionMetadata();
+        $metadata->setStatusCode($options['status_code'] ?? 500);
+        $metadata->setHidden($options['hidden'] ?? true);
+        $metadata->setLoggable($options['loggable'] ?? true);
+        $metadata->setLogLevel($options['log_level'] ?? 'debug');
+
+        return $metadata;
+    }
+
+    private function findOptionsByType(Throwable $e): array
+    {
+        foreach ($this->exceptionsMapping as $class => $options) {
+            if ($e instanceof $class) {
+                return $options;
+            }
+        }
+
+        return [];
+    }
+}
