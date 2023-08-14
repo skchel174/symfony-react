0674d5ad5e3417d8cbd8aac7f1946b57b2fbe8a6
Evgeny Kuzmin <skchel174@gmail.com>
2023-08-14 18:16:42 +0500

    Added exception handler to index file

--- public/index.php
+++ public/index.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 use App\ControllerResolver\ArgumentsResolver;
 use App\ControllerResolver\ControllerResolver;
 use App\DependencyInjection\ContainerFactory;
+use App\Event\ExceptionEvent;
 use App\Event\RequestEvent;
 use App\Event\ResponseEvent;
 use Psr\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\ErrorHandler\BufferingLogger;
+use Symfony\Component\ErrorHandler\ErrorHandler;
 use Symfony\Component\HttpFoundation\Request;
 
 const ENV = 'dev';
@@ -16,37 +19,51 @@ define('PROJECT_DIR', dirname(__DIR__));
 
 require_once PROJECT_DIR . '/vendor/autoload.php';
 
-$containerFactory = new ContainerFactory(DEBUG, ENV, PROJECT_DIR);
-$container = $containerFactory->createContainer();
+ErrorHandler::register(new ErrorHandler(new BufferingLogger(), DEBUG));
 
 $request = Request::createFromGlobals();
 
+$containerFactory = new ContainerFactory(DEBUG, ENV, PROJECT_DIR);
+$container = $containerFactory->createContainer();
+
 $eventDispatcher = $container->get(EventDispatcherInterface::class);
 
-$requestEvent = new RequestEvent($request);
-$eventDispatcher->dispatch($requestEvent);
+try {
+    $requestEvent = new RequestEvent($request);
+    $eventDispatcher->dispatch($requestEvent);
 
-if ($requestEvent->hasResponse()) {
-    $response = $requestEvent->getResponse();
+    if ($requestEvent->hasResponse()) {
+        $response = $requestEvent->getResponse();
+        $response->send();
+        exit;
+    }
+
+    /** @var ControllerResolver $controllerResolver */
+    $controllerResolver = $container->get(ControllerResolver::class);
+
+    /** @var ArgumentsResolver $controllerResolver */
+    $argumentsResolver = $container->get(ArgumentsResolver::class);
+
+    if (!$controller = $controllerResolver->getController($request)) {
+        throw new RuntimeException(sprintf('Not found controller for path "%s"', $request->getUri()));
+    }
+
+    $arguments = $argumentsResolver->getArguments($request, $controller);
+    $response = $controller(...$arguments);
+
+    $responseEvent = new ResponseEvent($request, $response);
+    $eventDispatcher->dispatch($responseEvent);
+    $response = $responseEvent->getResponse();
     $response->send();
-    exit;
+} catch (Throwable $e) {
+    $exceptionEvent = new ExceptionEvent($e, $request);
+    $eventDispatcher->dispatch($exceptionEvent);
+
+    if (!$exceptionEvent->hasResponse()) {
+        throw $e;
+    }
+
+    $response = $exceptionEvent->getResponse();
+    $response->send();
 }
 
-/** @var ControllerResolver $controllerResolver */
-$controllerResolver = $container->get(ControllerResolver::class);
-
-/** @var ArgumentsResolver $controllerResolver */
-$argumentsResolver = $container->get(ArgumentsResolver::class);
-
-if (!$controller = $controllerResolver->getController($request)) {
-    throw new RuntimeException(sprintf('Not found controller for path "%s"', $request->getUri()));
-}
-
-$arguments = $argumentsResolver->getArguments($request, $controller);
-$response = $controller(...$arguments);
-
-$responseEvent = new ResponseEvent($request, $response);
-$eventDispatcher->dispatch($responseEvent);
-$response = $responseEvent->getResponse();
-
-$response->send();
