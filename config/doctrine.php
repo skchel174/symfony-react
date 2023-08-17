7ef46f80d8e645e9e9600f92ee2faf11eeb6e0b0
Evgeny Kuzmin <skchel174@gmail.com>
2023-08-17 18:32:19 +0500

    Added EntityManager initialization from factory to config file

--- config/doctrine.php
+++ config/doctrine.php
@@ -2,7 +2,8 @@
 
 declare(strict_types=1);
 
-use App\Doctrine\EntityManagerFactory;
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\DriverManager;
 use Doctrine\Migrations\Configuration\Configuration as MigrationsConfiguration;
 use Doctrine\Migrations\Configuration\EntityManager\ExistingEntityManager;
 use Doctrine\Migrations\Configuration\Migration\ExistingConfiguration;
@@ -11,51 +12,70 @@ use Doctrine\Migrations\Metadata\Storage\TableMetadataStorageConfiguration;
 use Doctrine\Migrations\Tools\Console\Command\DiffCommand;
 use Doctrine\Migrations\Tools\Console\Command\ExecuteCommand;
 use Doctrine\Migrations\Tools\Console\Command\MigrateCommand;
+use Doctrine\ORM\Configuration;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Driver\AttributeDriver;
+use Doctrine\ORM\Mapping\UnderscoreNamingStrategy;
 use Doctrine\ORM\Tools\Console\Command\GenerateProxiesCommand;
 use Doctrine\ORM\Tools\Console\Command\SchemaTool\DropCommand;
 use Doctrine\ORM\Tools\Console\Command\ValidateSchemaCommand;
 use Doctrine\ORM\Tools\Console\EntityManagerProvider;
 use Doctrine\ORM\Tools\Console\EntityManagerProvider\SingleManagerProvider;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 return static function (ContainerConfigurator $container) {
-    $parameters = $container->parameters();
-
-    $parameters->set('doctrine.default', [
-        'cache_dir' => '%app.cache_dir%/doctrine/cache',
-        'proxy_dir' => '%app.cache_dir%/doctrine/proxy',
-
-        'metadata' => [
-            '%app.project_dir%/src/Entity',
-        ],
-
-        'connection' => [
-            'driver' => 'pdo_mysql',
-            'host' => getenv('MYSQL_HOST'),
-            'port' => getenv('MYSQL_PORT'),
-            'dbname' => getenv('MYSQL_DATABASE'),
-            'user' => getenv('MYSQL_USER'),
-            'password' => getenv('MYSQL_PASSWORD'),
-        ],
-    ]);
-
     $services = $container->services();
 
+    // EntityManager
     $services->set(EntityManagerInterface::class, EntityManager::class)
-        ->factory(service(EntityManagerFactory::class))
-        ->args(['%doctrine.default%'])
+        ->args([
+            service(Connection::class),
+            service(Configuration::class),
+        ])
         ->public();
 
-    $services->set(EntityManagerFactory::class)
-        ->args(['%app.env%']);
+    // DBAL\Connection
+    $services->set(Connection::class)
+        ->factory([DriverManager::class, 'getConnection'])
+        ->args([
+            [
+                'driver' => 'pdo_mysql',
+                'host' => getenv('MYSQL_HOST'),
+                'port' => getenv('MYSQL_PORT'),
+                'dbname' => getenv('MYSQL_DATABASE'),
+                'user' => getenv('MYSQL_USER'),
+                'password' => getenv('MYSQL_PASSWORD'),
+            ],
+            service(Configuration::class),
+        ])
+        ->public();
 
+    // Configuration
+    $services->set(Configuration::class)
+        ->call('setQueryCache', [service('doctrine.cache')])
+        ->call('setResultCache', [service('doctrine.cache')])
+        ->call('setMetadataCache', [service('doctrine.cache')])
+        ->call('setProxyDir', ['%app.cache_dir%/doctrine/proxy'])
+        ->call('setProxyNamespace', ['DoctrineProxies'])
+        ->call('setAutoGenerateProxyClasses', [true])
+        ->call('setNamingStrategy', [service(UnderscoreNamingStrategy::class)])
+        ->call('setMetadataDriverImpl', [service(AttributeDriver::class)]);
+
+    $services->set('doctrine.cache', FilesystemAdapter::class)
+        ->arg('$directory', '%app.cache_dir%/doctrine/cache');
+
+    $services->set(UnderscoreNamingStrategy::class);
+
+    $services->set(AttributeDriver::class)
+        ->args([['%app.project_dir%/src/Entity']]);
+
+    // CLI commands dependencies
     $services->set(EntityManagerProvider::class, SingleManagerProvider::class)
         ->args([service(EntityManagerInterface::class)]);
 
-    // Migrations
     $services->set(DependencyFactory::class)
         ->factory([null, 'fromEntityManager'])
         ->args([
